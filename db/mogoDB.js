32ff311b7fa51d3184615d04cdedad2c0b074f1e
Mahabsd <Mahabsd@gmail.com>
2021-01-13 11:16:50 +0100

    Create a Project Node & Install all Necessary Modules

---
+++ db/mogoDB.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/projetclinique', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(result =>{
+    console.log("connected to dataBase");
+}).catch(err =>{console.log(err);});
---
+++ server.js
@@ -0,0 +1,22 @@
+var createError = require('http-errors');
+var express = require('express');
+require('./db/mogoDB')
+const hostname = "127.0.0.1";
+const port = 3000;
+var bodyParser = require('body-parser');
+var app = express();
+var cors = require('cors')
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(cors())
+
+app.use((req, res) => {
+    res.json({ message: 'Votre requête a bien été reçue !' }); 
+ });
+ 
+  
+app.listen(port,hostname, ()=>{
+    console.log("server is running at http://"+hostname+":"+port);
+});
