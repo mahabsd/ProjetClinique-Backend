2c06eede9fcf43309f902fad279b2ca8e9c88d3e
Mahabsd <Mahabsd@gmail.com>
2021-02-22 21:54:39 +0100

    notification get and post

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -1,92 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const Maintenance = require("../models/maitenance");
-const Conge = require("../models/conge");
-const Sms = require("../models/sms")
-const Chat = require("../models/chat")
-
+const Notif = require("../models/notification");
+const passport = require('passport');
 
 
 // Send Notification API
-router.post('/send-notification', (req, res) => {
-    const notify = {data: req.body};
-    const io = req.router.get('io');
-    io.emit('notification', notify); // Updates Live Notification
-    res.send(notify);
+router.post('/send-notification', passport.authenticate('bearer', { session: false }), (req, res) => {
+    const notify = { data: req.body };
+    const io = req.app.get('io');
+    io.emit('notification', notify);
+    let notif = new Notif(req.body);
+    notif.save().then(item => {
+        res.json({ message: 'notif sent successfully ' });
+    }).catch(err => {
+        console.log(err);
+    });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var notifs = {
-    maint: '',
-    conge: '',
-    sms: '',
-    chat: '',
-    rendezvous:'',
-}
-router.get('/notifications', (req, res) => {
-    //maintenace
-    Maintenance.find({}, function (err, maintenances) {
-        maintenances.forEach(function (maintenance) {
-            if (maintenance.notifications == false) {
-
-                notifs = {
-                    maint: "you have a new maintenance",
-                }
-            }
-        })
-       
-    }).then(()=>{
-        res.status(200).send(notifs)
+//get all notif
+router.get('/get-notification/',passport.authenticate('bearer', { session: false }), (req, res) =>{
+    Notif.find().then(data => {
+        res.status(200).json(data);
     }).catch(err => res.status(400).json('Error: ' + err));
-    //conge
-    // Conge.find({}, function (err, conge) {
-    //     conge.forEach(function (conge) {
-    //         if (conge.notifications == false) {
-    //             notifs = {
-    //                 conge: "you have a new days leaves request to validate",
-    //             }
-    //         }
-    //     });
-    // })
-    //SMS
-    // Sms.find({}, function (err, sms) {
-    //     sms.forEach(function (sms) {
-    //         if (sms.notifications == false) {
-    //             notifs = {
-    //                 sms: "you have a new sms to validate",
-    //             }
-    //         }
-    //     });
-    // })
-
-    //chat
-    // Chat.find({}, function (err, chat) {
-    //     chat.forEach(function (message) {
-    //         if (message.messages.notifications == false) {
-    //             notifs = {
-    //                 chat: "you have a new message",
-    //             }
-    //         }
-    //     });
-    // })
-
-
 })
 module.exports = router;
