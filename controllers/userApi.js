2fdf9b7f706080f7bde0985a6f532fbf44d30c2d
Mahabsd <Mahabsd@gmail.com>
2021-03-10 15:10:50 +0100

    change port and path img from localhost to https://okbaclinical.herokuapp.com/

--- controllers/userApi.js
+++ controllers/userApi.js
@@ -45,7 +45,7 @@ router.post('/user/add/', [upload.single('image'), passport.authenticate('bearer
         settings: JSON.parse(formData.settings),
     })
     if (req.file) {
-        user.profile.image = "http://localhost:3000/api/"+ req.file.path
+        user.profile.image = "https://okbaclinical.herokuapp.com/api/"+ req.file.path
     }
     bcrypt.hash(user.password, 10, function (err, hash) {
         user.password = hash;
@@ -99,7 +99,7 @@ router.put('/user/update/:id', [upload.single('image'), passport.authenticate('b
     bcrypt.hash(user.password, 10, function (err, hash) {
         user.password = hash;
         if (req.file) {
-            user.profile.image = "https://okbaclinical.herokuapp.com/api/"+req.file.path
+            user.profile.image = "https://okbaclinical.herokuapp.com/"+req.file.path
         }
         User.findByIdAndUpdate(req.params.id, user).then(function (user) {
             res.status(200).json(
